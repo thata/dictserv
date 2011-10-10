ce3c3b361421c4ad6b687f47f331de72a12a6c45
takashi hatakeyama <takashi.hatakeyama@gmail.com>
2011-10-10 22:41:17 +0900

    add rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dictserv::Application.config.secret_token = '474c80c7d08a6d4e5c11e0f40c70a1265685f24a91a885b7070fa3d49c255484b33ff45e73c44d528557f007936f821787003f55c46decfb871dbfd2e634785d'
