eb4383a7d8063aba86533de3652b667f0f46431c
takashi hatakeyama <takashi.hatakeyama@gmail.com>
2011-10-11 22:51:40 +0900

    ひととおり実装した

--- app/controllers/dictionary_controller.rb
+++ app/controllers/dictionary_controller.rb
@@ -2,7 +2,17 @@ class DictionaryController < ApplicationController
   layout nil
 
   def lookup
+    if params[:word].blank?
+      render text: "", status: 400
+      return
+    end
+
     dict = Dictionary.new
-    render :json => dict.lookup(params[:word])
+    result = dict.lookup(params[:word])
+    if result
+      render :json => dict.lookup(params[:word])
+    else
+      render text: "", status: 404
+    end
   end
 end
--- spec/requests/lookup_dictionary_spec.rb
+++ spec/requests/lookup_dictionary_spec.rb
@@ -1,12 +1,42 @@
 # -*- encoding: utf-8 -*-
 require 'spec_helper'
 
+RSpec::Matchers.define :have_entry do |word, translation_regex|
+  match do |subject|
+    j = JSON.parse(subject)
+    val = j[word]
+    val =~ translation_regex
+  end
+end
+
 describe "Lookup dictionary" do
-  describe "GET /dictionary/lookup" do
+  describe "GET /dictionary/lookup?word=hello" do
     it "単語を検索できること" do
       visit "/dictionary/lookup?word=hello"
-      j = JSON.parse page.source
-      j.should == { "hello" => "こんにちは" }
+      page.source.should have_entry("hello", /こんにちは/)
+    end
+  end
+
+  describe "GET /dictionary/lookup?word=xxxxxx" do
+    it "単語を検索できること" do
+      visit "/dictionary/lookup?word=xxxxxx"
+      page.status_code.should == 404
+    end
+  end
+
+  describe "GET /dictionary/lookup?word=" do
+    it "400 BadRequest が返ること" do
+      visit "/dictionary/lookup?word="
+      page.status_code.should == 400
+    end
+  end
+
+  describe "GET /dictionary/lookup?" do
+    it "400 BadRequest が返ること" do
+      visit "/dictionary/lookup"
+      page.status_code.should == 400
     end
   end
 end
+
+
